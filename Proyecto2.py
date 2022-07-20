4bbdac4d1cd6e40839f1467d100eb84b35af3c36
Kevin C <kevin.dany.contreras@gmail.com>
2022-07-20 16:30:16 -0500

    Algoritmo Jerarquico

--- Proyecto2.py
+++ Proyecto2.py
@@ -1,6 +1,6 @@
 from cProfile import label
 from collections import defaultdict
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.metrics import silhouette_score,precision_recall_fscore_support
 import matplotlib.pyplot as plt
 import numpy as np
@@ -80,6 +80,81 @@ def calcularKmeans(archivo, clusters):
         metricas=precision_recall_fscore_support(Clases,prediccion)
         printMetricas(metricas,silhouette_score(datos,kmeans.labels_))
 
+def calcularJeerarquico(archivo, clusters):
+    #Archivo=open('Datos1.csv','r')
+    try:
+        Archivo = open(archivo,'r')
+        print(f'Jerarquico \nCon el archivo {archivo} \nk={clusters} ')
+    except:
+        print("Archivo invalido")
+    else:
+        datos = []
+        cont = 1
+        Clases = []
+
+        with Archivo:
+            for x in Archivo.readlines():#leer los datos
+                if len(x)>2:
+                    if cont==1:
+                        Atributos=x.replace('\n','').split(',')
+                        cont+=1
+                    else:
+                        atrib=x.replace('\n','').split(',')
+                        Clases.append(atrib[len(atrib)-1])
+                        datos.append(list(map(float,atrib[:-1])))
+
+        jerarquico = AgglomerativeClustering(n_clusters=clusters).fit(datos)#500 es default
+        #print(Clases)
+        prediccion=jerarquico.labels_
+        #print(kmeans.labels_)
+
+        #print(kmeans.cluster_centers_)
+        #todo precision y recall
+        #clase labels
+        diccionario = defaultdict(int)
+        for x in Clases:
+            diccionario[x]=[0]*clusters
+        for x, y in zip(Clases, prediccion):
+            #diccionario[str(y) + x] += 1
+            diccionario[x][y]+=1
+            #print(x,y)
+        
+        
+        if clusters==2:
+            #print(diccionario)    
+            #print(Clases)
+            for i in diccionario:
+                #print(i)        
+                mayor=-1  
+                tmp=-1
+                for j in range(len(diccionario[i])):
+                    if mayor<diccionario[i][j]:
+                        mayor=diccionario[i][j]
+                        tmp=j
+                #print(tmp)
+                Clases=cambairDatos(Clases,i,tmp)
+            
+            #print(prediccion)
+            #print(Clases)
+        elif clusters==3:
+            #k3Todas
+            print(diccionario)    
+            print(Clases)
+            print(prediccion)
+            Clases=cambairDatos(Clases,"Profundo",0)
+            Clases=cambairDatos(Clases,"Superficial",1)
+            Clases=cambairDatos(Clases,"Logro",2)
+            print(Clases)
+            #k3Metricas
+
+            #k3pregutnas
+
+
+
+        # print(Clases)
+        metricas=precision_recall_fscore_support(Clases,prediccion)
+        printMetricas(metricas,silhouette_score(datos,jerarquico.labels_))
+
 
 def printMetricas(metricas,silhoute):
     print("---------------------------------------------------------------------------------------------")
@@ -95,9 +170,10 @@ def cambairDatos(arreglo,datoOriginal, datoCambiar):
     return arreglo
 
 #calcularKmeans('K2Todas.csv',2)
+calcularJeerarquico('K2Todas.csv',2)
 # calcularKmeans('K2Metricas.csv',2)
 # calcularKmeans('K2Preguntas.csv',2)
 
-calcularKmeans('K3Todas.csv',3)
+#calcularKmeans('K3Todas.csv',3)
 # calcularKmeans('K3Metricas.csv',3)
 # calcularKmeans('K3Preguntas.csv',3)
